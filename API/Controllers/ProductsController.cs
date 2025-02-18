16a474f94615bc65612c081dbf29a50059282a00
Mithun M <mithunm306@outlook.com>
2025-02-19 02:12:37 +0530

    Initial Commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext storeContext)
+        {
+            _context = storeContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            if (products == null)
+            {
+                return NotFound();
+            }
+            return Ok(products);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+    }
+}
